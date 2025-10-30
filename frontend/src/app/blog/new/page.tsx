e33f50e470fe76dd9096074a80d6728d906e9a4d
井上頌英 <inoueshouei@fuck-mini.local>
2025-10-30 22:38:47 +0900

    投稿完了したらtopページ

--- frontend/src/app/blog/new/page.tsx
+++ frontend/src/app/blog/new/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import BlogForm from "@/app/components/BlogForm";
 import { useBlogActions } from "@/hooks/useBlogActions";
+import { useRouter } from "next/navigation";
 
 export type Blog = {
 	id: number;
@@ -17,18 +18,20 @@ const Page = () => {
 	const [inputContent, setInputContent] = useState("");
 	const [isPosted, setIsPosted] = useState(false);
 	const { postBlog } = useBlogActions();
+	const router = useRouter();
 
 	const handlePost = async (title: string, content: string) => {
 		await postBlog(title, content);
 		setIsPosted(true);
 		setInputTitle("");
 		setInputContent("");
+		router.push("/");
 	};
 
 	return (
-		<div className="max-w-3xl mx-auto mt-12 space-y-6 text-center">
+		<div className='max-w-3xl mx-auto mt-12 space-y-6 text-center'>
 			{isPosted ? (
-				<div className="text-green-600 font-bold text-xl">完了しました</div>
+				<div className='text-green-600 font-bold text-xl'>完了しました</div>
 			) : (
 				<BlogForm
 					inputTitle={inputTitle}
